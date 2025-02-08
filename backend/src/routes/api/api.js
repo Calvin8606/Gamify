75e2ac481c08beca74ac17542a50476f94a5bbf7
Calvin <calvin71203@gmail.com>
2025-02-08 11:18:10 -0500

    Merge branch 'main' of https://github.com/Calvin8606/Gamify

--- backend/src/routes/api/api.js
+++ backend/src/routes/api/api.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
 const userRouter = require("./user");
+const profileRouter = require("./profile");
 
 const apiRouter = express.Router();
 apiRouter.use("/user", userRouter);
+apiRouter.use("/profile", profileRouter);
 
 module.exports = apiRouter;
--- backend/src/routes/api/profile.js
+++ backend/src/routes/api/profile.js
@@ -1,5 +1,9 @@
 const { requiresAuth } = require("express-openid-connect");
 
-app.get("/profile", requiresAuth(), (req, res) => {
+const profileRouter = express.Router();
+
+profileRouter.get("/profile", requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
+
+module.exports = profileRouter;
