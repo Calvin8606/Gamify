22d933719eeb492eae629e830a0cc808a9a3b156
Calvin <calvin71203@gmail.com>
2025-02-08 09:31:51 -0500

    user schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
+  firstName: { required: true, type: String, trim: true },
+  lastName: { required: true, type: String, trim: true },
+  email: { required: true, type: String, trim: true },
+  password: { required: true, type: String, trim: true },
+  totalScore: { type: Number, required: false, default: 0 },
+  badges: { type: Array, required: false, default: [] },
 });
 
 module.exports = mongoose.model("user", userSchema);
