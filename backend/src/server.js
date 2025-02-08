98e87e32b03be2f89fe25d1f1e1f627fa26d21f1
Holden Casey <me@holdencasey.com>
2025-02-08 09:26:25 -0500

    adds db to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,17 @@
 // CONSTANTS
 const express = require("express");
+const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
 const app = express();
 const port = 4781;
+connectDB();
 app.use(bodyParser.json());
 app.use(cors());
 
 // PORT CONNECTION
 app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+    console.log(`Listening on port: ${port}`);
 });
