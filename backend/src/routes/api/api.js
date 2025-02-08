1d8260928e5ad1bcd2c087ac74255ce3e7906c27
Holden Casey <me@holdencasey.com>
2025-02-08 13:15:35 -0500

    adds ebitda calculation

--- backend/src/routes/api/api.js
+++ backend/src/routes/api/api.js
@@ -4,11 +4,13 @@ const userRouter = require("./user");
 const profileRouter = require("./profile");
 const balanceSheetRouter = require("./balanceSheet");
 const incomeStatementRouter = require("./incomeStatement");
+const calcRouter = require("./calc");
 
 const apiRouter = express.Router();
 apiRouter.use("/user", userRouter);
 apiRouter.use("/profile", profileRouter);
 apiRouter.use("/balanceSheet", balanceSheetRouter);
 apiRouter.use("/incomeStatement", incomeStatementRouter);
+apiRouter.use("/calc", calcRouter);
 
 module.exports = apiRouter;
