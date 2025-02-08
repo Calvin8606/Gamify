c8b8dbae23ad61625b23c101171dd13c7eed21d9
Calvin <calvin71203@gmail.com>
2025-02-08 15:09:57 -0500

    indexcss changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </div>
   );
