694cba99a215ae25aec2c31d2504329bb4e08f4f
Jason <jql58922@uga.edu>
2025-02-08 15:15:58 -0500

    home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
+      
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </div>
   );
