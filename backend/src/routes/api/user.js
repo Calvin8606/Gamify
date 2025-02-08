c839e33da9185382658afa0c192aa6f7d128c33b
Holden Casey <me@holdencasey.com>
2025-02-08 09:33:17 -0500

    adds userRouter

---
+++ backend/src/routes/api/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const User = require("../../models/user");
+
+const userRouter = express.Router();
+
+// POST /api/user
+router.post("/", async (req, res) => {
+    try {
+        const { firstName, lastName, email, password, totalScore, badges } =
+            req.body;
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password,
+            totalScore,
+            badges,
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating user",
+            error: error.message,
+        });
+    }
+});
+
+module.exports = userRouter;
