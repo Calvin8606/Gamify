c9b8a1d63016c0f283fe9f233bdb4491b9bcde14
Calvin <calvin71203@gmail.com>
2025-02-08 11:34:11 -0500

    login buttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const authDomain = process.env.AUTH0_DOMAIN;
+const authId = process.env.AUTH0_ID;
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain={authDomain}
+    clientId={authId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>
+);
