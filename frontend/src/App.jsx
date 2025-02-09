5a844fa66b4e71fd4361239bee642132e8d756ac
Jason <jql58922@uga.edu>
2025-02-08 20:05:15 -0500

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import InfoPage from "./pages/InfoPage";
 import Quiz from "./pages/Quiz";
 import BalanceSheetVisualization from "./pages/BalanceSheetVisualization";
 import EbitdaVisualization from "./pages/EbitdaVisualization";
@@ -12,7 +14,7 @@ const testUserId = "67a780bea849cd0d0fcd62f8";
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/quiz" element={<Quiz userId={testUserId}/>} />
@@ -20,6 +22,7 @@ const App = () => {
         <Route path="/ebitda-visualization" element={<EbitdaVisualization />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/infopage" element={<InfoPage />} /> 
       </Routes>
     </div>
   );
